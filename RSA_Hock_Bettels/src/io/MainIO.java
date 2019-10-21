ceb5ebb9faff1e358fcee76f594dc5198da73b66
nikla <hockni@uni-hildesheim.de>
2019-10-21 13:16:31 +0200

    Erster Test

--- RSA_Hock_Bettels/src/io/MainIO.java
+++ RSA_Hock_Bettels/src/io/MainIO.java
@@ -2,37 +2,63 @@ package io;
 
 import java.util.Scanner;
 import java.lang.Math;
+import math.Calculation;
+import encode.Encode;
 
 public class MainIO {
 
 	private int mFirstValue;
 	private int mSecondValue;
+	static Calculation calc = new Calculation();
 	
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Geben Sie die erste Zahl ein: ");
-		int lFirstValue = sc.nextInt();
+		long lFirstValue = sc.nextLong();
 		sc.nextLine();
 		System.out.println("Geben Sie die zweite Zahl ein: ");
-		int lSecondValue = sc.nextInt();
+		long lSecondValue = sc.nextLong();
 		sc.nextLine();
 		
+		//MainIO main = new MainIO();
+		
+//		while(!main.inputValidation(lFirstValue, lSecondValue)) {
+//			System.out.println("Versuche es nochmal.");
+//			
+//			System.out.println("Geben Sie die erste Zahl ein: ");
+//			lFirstValue = sc.nextInt();
+//			sc.nextLine();
+//			
+//			System.out.println("Geben Sie die zweite Zahl ein: ");
+//			lSecondValue = sc.nextInt();
+//			sc.nextLine();
+//			
+//		}
+		
+		//Ab hier geht es mit Long nicht mehr.
+		if(calc.calculateGGT(lFirstValue, lSecondValue) == (long)1) {
+			long n = lFirstValue*lSecondValue;
+			System.out.println(calc.calculatePhiFunction(n));
+		}
 		System.out.println("Geben Sie nun den zu verschlüsselnden Text ein: ");
 		String lPlainText = sc.nextLine();
-		
 		sc.close();
+		
+		Encode encoder = new Encode();
+		encoder.encodeText(lPlainText);
+		
 	}
 	
-	private boolean inputValidation(int aFirstValue, int aSecondValue) {
+	private boolean inputValidation(long aFirstValue, long aSecondValue) {
 		
 		boolean isValid = false;
 		
-		if ((Math.pow(2, 32) < aFirstValue && aFirstValue < Math.pow(2, 64)) 
-				&& (Math.pow(2, 32) < aSecondValue && aSecondValue < Math.pow(2, 64)))
-		{
-			if (isRelativelyPrime(aFirstValue, aSecondValue))
+		if ((Math.pow(2, 32) <= aFirstValue && aFirstValue < Math.pow(2, 64)) 
+				&& (Math.pow(2, 32) < aSecondValue && aSecondValue <= Math.pow(2, 64)))
+		{	
+			if (calc.isPrimeNumber(aFirstValue, aSecondValue))
 			{
 				isValid = true;
 			}
@@ -41,23 +67,23 @@ public class MainIO {
 		return isValid;	
 	}
 	
-	private boolean isRelativelyPrime(int aFirstValue, int aSecondValue) {
-		
-		boolean isRelPrime = false;
-		
-		return isRelPrime;
-		
-	}
-	
-	private int calculatePhiFunction(int aFirstValue, int aSecondValue)
-	{
-		int n = aFirstValue * aSecondValue;
-		
-		int phiofN = 0;
-		
-		return phiofN;
-		
-	}
+//	private boolean isRelativelyPrime(int aFirstValue, int aSecondValue) {
+//		
+//		boolean isRelPrime = false;
+//		
+//		return isRelPrime;
+//		
+//	}
+//	
+//	private int calculatePhiFunction(int aFirstValue, int aSecondValue)
+//	{
+//		int n = aFirstValue * aSecondValue;
+//		
+//		int phiofN = 0;
+//		
+//		return phiofN;
+//		
+//	}
 
 	public int getFirstValue() {
 		return mFirstValue;
@@ -76,24 +102,3 @@ public class MainIO {
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
